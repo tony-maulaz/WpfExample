5102c0433fad09f737630dd438883ab4ac1b5b7f
Maulaz Tony <tony.maulaz@heig-vd.ch>
2021-06-29 22:08:45 +0200

    Add code example

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace WpfExample
 {
@@ -20,9 +21,163 @@ namespace WpfExample
     /// </summary>
     public partial class MainWindow : Window
     {
+        ViewModel vm = new ViewModel();
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = vm;
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            vm.SetControl(1);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            vm.SetControl(2);
+        }
+    }
+
+    public class ViewModel : NotifyBaseClass
+    {
+        public ObservableCollection<Person> Persons { get; private set; }
+
+        public Test test { get; set; }
+
+        public ViewModel()
+        {
+            test = new Test();
+            test.Value = 0;
+            Persons = new ObservableCollection<Person>();
+
+            Persons.Add(new Person(12, "Bob"));
+            Persons.Add(new Person(1, "Alice"));
+            Persons.Add(new Person(5, "Jean"));
+
+            selectedPerson = Persons[1];
+        }
+
+        public void SetControl(int id)
+        {
+            if(id == 1)
+            {
+                CurrentControl = new UserControl1();
+            }
+            else
+            {
+                CurrentControl = new UserControl2();
+            }
+            CurrentControl.DataContext = SelectedPerson;
+        }
+
+        private ICommand clickCommand;
+        public ICommand ClickCommand
+        {
+            get
+            {
+                //return clickCommand ?? (clickCommand = new CommandHandler(() => MyAction(), () => CanExecute()));
+                // lambda
+                return clickCommand ?? (clickCommand = new CommandHandler(() => MyAction(), () => { return ButtonEnabled; }));
+            }
+        }
+        /* possible de mettre une méthode
+        public bool CanExecute
+        {
+            get
+            {
+                return ButtonEnabled;
+            }
+        }
+        */
+
+        public void MyAction()
+        {
+            
+            test.Name = "Test 1";
+            test.Value++;
+        }
+
+        private Control currentControl;
+        public Control CurrentControl
+        {
+            get { return currentControl; }
+            set
+            {
+                if( currentControl != value)
+                {
+                    currentControl = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private bool buttonEnabled;
+        public bool ButtonEnabled
+        {
+            get { return buttonEnabled; }
+            set
+            {
+                if (value != buttonEnabled)
+                {
+                    buttonEnabled = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private Person selectedPerson;
+        public Person SelectedPerson
+        {
+            get { return selectedPerson; }
+            set
+            {
+                if(selectedPerson != value)
+                {
+                    selectedPerson = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+
+    public class Test : NotifyBaseClass
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if( value != this.name)
+                {
+                    this.name = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private double value;
+        public double Value
+        {
+            get { return value; }
+            set
+            {
+                this.value = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+
+    public class Person : NotifyBaseClass
+    {
+        public Person(int id, string name)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public string Name { get; set; }
+        public int Id { get; set; }
     }
 }
---
+++ Notify.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace WpfExample
+{
+    public class CommandHandler : ICommand
+    {
+        private Action _action;
+        private Func<bool> _canExecute;
+
+        public CommandHandler(Action action, Func<bool> canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute.Invoke();
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+
+    public class NotifyBaseClass : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
